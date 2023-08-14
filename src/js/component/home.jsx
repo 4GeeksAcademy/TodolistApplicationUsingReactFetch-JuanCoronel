2787aa37523c7dffe721690ce6680e0d07815061
Juan Coronel <juanocoronell@gmail.com>
2023-08-14 20:10:52 +0000

    fim

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -44,8 +44,10 @@ const Home = () => {
 <div className="center-div border border-danger bg-danger text-light w-50 m-3 p-2 mb-4"> ¡Ninguna tarea agregada para este mes!</div>
         ) : (
           tareas.map((tarea, indice) => (
-            <li className="list-group-item list-group-item-secondary pt-2" key={indice}>
-              {tarea}
+            <li className="list-group-item list-group-item-secondary pt-2" key={indice}> <strong> <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-right" viewBox="0 0 16 16">
+            <path fill-rule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z"/>
+          </svg></strong> 
+              { tarea}
               <button className="btn btn-danger float-end" onClick={() => eliminarTarea(indice)}> <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-trash3-fill" viewBox="0 0 16 16">
   <path d="M11 1.5v1h3.5a.5.5 0 0 1 0 1h-.538l-.853 10.66A2 2 0 0 1 11.115 16h-6.23a2 2 0 0 1-1.994-1.84L2.038 3.5H1.5a.5.5 0 0 1 0-1H5v-1A1.5 1.5 0 0 1 6.5 0h3A1.5 1.5 0 0 1 11 1.5Zm-5 0v1h4v-1a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5ZM4.5 5.029l.5 8.5a.5.5 0 1 0 .998-.06l-.5-8.5a.5.5 0 1 0-.998.06Zm6.53-.528a.5.5 0 0 0-.528.47l-.5 8.5a.5.5 0 0 0 .998.058l.5-8.5a.5.5 0 0 0-.47-.528ZM8 4.5a.5.5 0 0 0-.5.5v8.5a.5.5 0 0 0 1 0V5a.5.5 0 0 0-.5-.5Z"/>
 </svg></button>
